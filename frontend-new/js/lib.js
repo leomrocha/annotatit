bb3bc00d5dbca7627ca0c24b853b0f6466d06ec9
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2013-07-12 11:15:49 +0200

    Changed player_slider for actual API that has to be used. TODO test and complete with the API.

--- frontend-new/js/lib.js
+++ frontend-new/js/lib.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
 		])
 	);
 
-	var player_slider = $("[data-name=player-slider]");
+	//var player_slider = $("[data-name=player-slider]");
 
 	/** @type {viewModel} View for rendering a Lorem data into the view */
 	var syncCommentsViewModel = new SyncCommentsViewModel(
@@ -18,14 +18,15 @@ $( document ).ready(function() {
 	    {comment: "Lorem ipsum dolor sit amet,"},
 	    {comment: "Lorem ipsum dolor sit amet,"},
 	    {comment: "Lorem ipsum dolor sit amet,"}
-		]),player_slider
+		])//,player_slider
 	);
-
+/*
 	player_slider.slider({
     range: "min",
     value: 0,
     min: 0,
     max: 120
+*/
   });
 
 	/** Binds commentsViewModel to the main section in the view */
