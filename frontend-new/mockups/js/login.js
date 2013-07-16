ee61ca0c5ffaabc8bd5142649e5e2c9db7761233
AkkaVoskuil <akkavoskuil@gmail.com>
2013-07-16 15:16:31 -0300

    Landing page and Login Pop-up done. Login Pop-up can be found in Login.js in js folder

---
+++ frontend-new/mockups/js/login.js
@@ -0,0 +1,38 @@
+document.write('<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">'+
+	    '<div class="myformy bred">'+
+            '<h3>Login to your Account</h3>'+
+                '<div class="form">'+
+                    '<!-- Login form (not working)-->'+
+                    '<form class="form-horizontal">'+
+                        
+                        '<div class="control-group">'+
+                        	'<label class="control-label" for="username">Username</label>'+
+                            	'<div class="controls">'+
+                                	'<input type="text" class="input-large" id="username">'+
+                                '</div>'+
+                        '</div>'+
+                        
+                        '<div class="control-group">'+
+                            '<label class="control-label" for="email">Password</label>'+
+                            '<div class="controls">'+
+                                '<input type="password" class="input-large" id="password">'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="control-group">'+
+                            '<div class="controls">'+
+                                '<label class="checkbox">'+
+                                '<input type="checkbox"> Remember me'+
+                                '</label>'+
+                            '</div>'+
+                        '</div>'+                                                                               
+                        
+                        '<div class="form-actions">'+
+                            
+                            '<button type="submit" class="btn">Login</button>&nbsp; &nbsp; | &nbsp &nbsp;'+
+                            '<button type="reset" class="btn">Reset</button>'+
+                        '</div>'+
+                	'</form>'+
+                    'Dont have an Account? <a href="register.html">Register</a>'+
+                '</div>'+ 
+            '</div>'+
+		                '</div>');
